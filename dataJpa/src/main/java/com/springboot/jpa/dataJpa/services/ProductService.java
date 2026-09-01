e396b399a92b012e0d7b05093e61d624bf58bafb
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-09-01 23:30:20 +0530

    Refactor ProductService to use DTOs for product operations

--- dataJpa/src/main/java/com/springboot/jpa/dataJpa/services/ProductService.java
+++ dataJpa/src/main/java/com/springboot/jpa/dataJpa/services/ProductService.java
@@ -1,9 +1,16 @@
 package com.springboot.jpa.dataJpa.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.springboot.jpa.dataJpa.Entities.Product;
+import com.springboot.jpa.dataJpa.Entities.ProductRequestDto;
+import com.springboot.jpa.dataJpa.Entities.ProductResponseDto;
 import com.springboot.jpa.dataJpa.Repository.ProductRespository;
 
 @Service
@@ -11,15 +18,76 @@ public class ProductService {
 	@Autowired
 	ProductRespository pr;
 	
-	  public Product saveProduct(Product product) {
+	  public ProductResponseDto saveProduct(ProductRequestDto RequestDto) {
 		  
-		  if(product!=null);
-		     pr.save(product);
-		  
-		  return product;
+		Product product=new Product();
+		product.setColor(RequestDto.getColor());
+		product.setId(RequestDto.getId());
+		product.setName(RequestDto.getName());
+		product.setPrice(RequestDto.getPrice());
+		product.setQuantity(RequestDto.getQuantity());
+		
+		pr.save(product);
+		
+		
+		 ProductResponseDto ResponseDto=new ProductResponseDto();
+		 ResponseDto.setColor(product.getColor());
+		 ResponseDto.setName(product.getName());
+		 ResponseDto.setId(product.getId());
+		 ResponseDto.setPrice(product.getPrice());
+		 ResponseDto.setQuantity(product.getQuantity());
+		return ResponseDto;
 	  }
-	public  List<Product> viewAllProducts() {
+	  
+	  
+	  public List<ProductResponseDto> saveAllProduct(List<ProductRequestDto> RequestDto) {
+		  List<Product> products=new ArrayList<>();
+		   for (ProductRequestDto request : RequestDto) {
+			  Product product=new Product();
+			   product.setColor(request.getColor());
+				product.setId(request.getId());
+				product.setName(request.getName());
+				product.setPrice(request.getPrice());
+				product.setQuantity(request.getQuantity());
+				products.add(product);	
+		}
+		   pr.saveAll(products);
+		   
+		   List<ProductResponseDto> ResponseDto=new ArrayList<>();
+		   for(Product product:products) {
+			   
+		   ProductResponseDto ResponseDtos=new ProductResponseDto();
+		   
+			 ResponseDtos.setColor(product.getColor());
+			 ResponseDtos.setName(product.getName());
+			 ResponseDtos.setId(product.getId());
+			 ResponseDtos.setPrice(product.getPrice());
+			 ResponseDtos.setQuantity(product.getQuantity());
+			 ResponseDto.add(ResponseDtos);
+		   }
+		   return ResponseDto;
+		   
+		   
+		  
+	  }
+	  
+	  public ProductResponseDto getProductById(int id) {
+		  Optional<Product> optional=pr.findById(id);
+		  Product product=optional.get();
+		  
+		  ProductResponseDto responseDto=new ProductResponseDto();
+		  responseDto.setId(product.getId());
+		  responseDto.setColor(product.getColor());
+		  responseDto.setName(product.getName());
+		  responseDto.setPrice(product.getPrice());
+		  responseDto.setQuantity(product.getQuantity());
+		  return responseDto;
+		  
+	  }
+	  
+public  List<Product> viewAllProducts() {
 		List<Product> list=pr.findAll();
 		return list;
 	  }
+
 }
