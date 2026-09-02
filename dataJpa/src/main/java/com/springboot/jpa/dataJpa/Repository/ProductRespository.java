a0a31fd428bf82768be00600cf09ede4f23634d7
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-09-02 23:28:27 +0530

    Enhance ProductRepository with custom query methods
    Added methods to find products by color and delete products by price.

--- dataJpa/src/main/java/com/springboot/jpa/dataJpa/Repository/ProductRespository.java
+++ dataJpa/src/main/java/com/springboot/jpa/dataJpa/Repository/ProductRespository.java
@@ -6,5 +6,6 @@ import com.springboot.jpa.dataJpa.Entities.Product;
 
 @Repository
 public interface ProductRespository extends JpaRepository<Product,Integer>{
-
+   public List<Product> findByColor(String color);
+    public List<Product> deleteByPrice(double price);
 }
