b43a7c79a8ec3d2718ab4969cc8ffefb802071e8
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-09-10 23:55:46 +0530

    Update Adhar.java

--- DataMapping/src/main/java/com/DataJpaMapping/DataMapping/Entity/Adhar.java
+++ DataMapping/src/main/java/com/DataJpaMapping/DataMapping/Entity/Adhar.java
@@ -1,11 +1,20 @@
 package com.DataJpaMapping.DataMapping.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Adhar {
+	@Id
   private Long id;
   private String fathersName;
+  
+  @OneToOne(mappedBy = "adhar")
+  private Person person;
 }
