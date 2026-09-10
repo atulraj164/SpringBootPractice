40e1cf5580cb2f7e84bd56c3ef9e224f3d324a68
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-09-10 23:55:09 +0530

    Add files via upload

---
+++ DataMapping/src/main/java/com/DataJpaMapping/DataMapping/Repository/AdharRepository.java
@@ -0,0 +1,9 @@
+package com.DataJpaMapping.DataMapping.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.DataJpaMapping.DataMapping.Entity.Adhar;
+
+public interface AdharRepository extends JpaRepository<Adhar,Long>{
+
+}
