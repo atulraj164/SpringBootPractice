82a28725cd15c2b14ba319a14c435397221f5e8b
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-09-10 23:56:14 +0530

    Enhance Person class with Adhar relationship
    Refactored Person class to include Adhar relationship and updated annotations.

--- DataMapping/src/main/java/com/DataJpaMapping/DataMapping/Entity/Person.java
+++ DataMapping/src/main/java/com/DataJpaMapping/DataMapping/Entity/Person.java
@@ -1,12 +1,25 @@
 package com.DataJpaMapping.DataMapping.Entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Person {
-
+     
+     @Id	
 	private Integer id;
 	private String name;
+	
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="adhar")
+	private Adhar adhar;
 }
