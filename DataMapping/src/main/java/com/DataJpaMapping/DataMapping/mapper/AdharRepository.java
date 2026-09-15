0f027e3feb7dd9bc1a30bf980b55ce2cc5d9661c
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-09-15 22:20:18 +0530

    Delete DataMapping/src/main/java/com/DataJpaMapping/DataMapping/mapper/AdharRepository.java

--- DataMapping/src/main/java/com/DataJpaMapping/DataMapping/mapper/AdharRepository.java
+++
@@ -1,9 +0,0 @@
-package com.DataJpaMapping.DataMapping.mapper;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.DataJpaMapping.DataMapping.Entity.Adhar;
-
-public interface AdharRepository extends JpaRepository<Adhar,Long>{
-
-}
