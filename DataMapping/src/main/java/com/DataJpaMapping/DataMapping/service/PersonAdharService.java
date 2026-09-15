efa1803a92ca89ab547b55a71831445b5d9e1ca8
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-09-15 22:19:42 +0530

    Refactor PersonAdharService to use DTOs and mappers
    Refactored PersonAdharService to use DTOs and mappers for better data handling. Added methods for adding, retrieving by ID, and listing persons with their Adhar information.

--- DataMapping/src/main/java/com/DataJpaMapping/DataMapping/service/PersonAdharService.java
+++ DataMapping/src/main/java/com/DataJpaMapping/DataMapping/service/PersonAdharService.java
@@ -1,10 +1,19 @@
 package com.DataJpaMapping.DataMapping.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.DataJpaMapping.DataMapping.Dto.PersonRequestDto;
+import com.DataJpaMapping.DataMapping.Dto.PersonResponseDto;
+import com.DataJpaMapping.DataMapping.Entity.Adhar;
 import com.DataJpaMapping.DataMapping.Entity.Person;
 import com.DataJpaMapping.DataMapping.Repository.AdharRepository;
 import com.DataJpaMapping.DataMapping.Repository.PersonRepository;
+import com.DataJpaMapping.DataMapping.mapper.PersonAdharMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,13 +22,39 @@ import lombok.RequiredArgsConstructor;
 public class PersonAdharService {
    
 	   private final PersonRepository personrepository;
-	   private final AdharRepository adharrepository;
+       private final PersonAdharMapper mapper;
+       
+       
 	   
-	   
-	   public Person AddPersonAdhar(Person person) {
-		   System.out.println(person.getAdhar().getFathersName()+"   "+person.getAdhar().getId());
-		   return personrepository.save(person);
+	   public PersonResponseDto AddPersonAdhar(PersonRequestDto request) {
+		      Person person=new Person();
+		      person=mapper.toPerson(request);
+		      person.setAdhar(mapper.toAdhar(request.getAdhar()));
+		      PersonResponseDto response=new PersonResponseDto();
+		      response=mapper.toPersonResponse( personrepository.save(person));
+		      response.setAdhar(mapper.toAdharResponse(person.getAdhar()));
+		      
+		      return response;
 	   }
+	   
+	   public PersonResponseDto getPersonAdharById(int id) {
+		   PersonResponseDto response=new PersonResponseDto();
+		   Person person= personrepository.findById(id).get();;
+		   response=mapper.toPersonResponse(person);
+		   response.setAdhar(mapper.toAdharResponse(person.getAdhar()));
+		   return response;
+	   }
+	   
+	   public PersonResponseDto getAllPersonAndAdhar(Integer i) {
+		   List<Person> person=new ArrayList<>();
+		   person=personrepository.findAll(PageRequest.of(5, i)).toList();
+		   List<PersonResponseDto>response=
+				   person.stream().map(n->mapper.toPersonResponse(n)).toList();
+		   
+		   response=person.stream().map(n->n.getAdhar()).map(n->mapper.toAdharResponse(n)).toList().to
+		   
+	   }
+	   
 	   
 	
 }
